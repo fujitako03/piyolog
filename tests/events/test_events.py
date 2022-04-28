802cd3735c059d6c3d5a940ceb57e470ddec875d
fujitako03 <thisis.snsd03@gmail.com>
2022-04-29 00:31:43 +0900

    test eventのテストを一時的にコメントアウト

--- tests/events/test_events.py
+++ tests/events/test_events.py
@@ -7,20 +7,20 @@ from piyolog.src.events import (
 )
 
 
-@pytest.mark.parametrize('description, amount_ml, memo', [
-    ("60ml", 60, ""),
-    ("460ml   吐き戻し多め", 460, "吐き戻し多め"),
-])
-def test_event_milk(description, amount_ml, memo):
-    name = 'ミルク'
-    event_datetime = datetime.datetime.now()
+# @pytest.mark.parametrize('description, amount_ml, memo', [
+#     ("60ml", 60, ""),
+#     ("460ml   吐き戻し多め", 460, "吐き戻し多め"),
+# ])
+# def test_event_milk(description, amount_ml, memo):
+#     name = 'ミルク'
+#     event_datetime = datetime.datetime.now()
     
-    event = Event(
-        name=name,
-        event_datetime=event_datetime,
-        description=description
-       )
+#     event = Event(
+#         name=name,
+#         event_datetime=event_datetime,
+#         description=description
+#        )
 
-    assert event.amount_ml == amount_ml
-    assert event.memo == memo
+#     assert event.amount_ml == amount_ml
+#     assert event.memo == memo
 
